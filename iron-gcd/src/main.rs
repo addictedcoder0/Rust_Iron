a5fccf7a2c140f68ae594396b63ef4bda29be6e6
abhishek <abhishek-2863@abhishek-2863.csez.zohocorpin.com>
2017-02-01 04:02:03 +0530

    iron-gcd-with-router

--- iron-gcd/src/main.rs
+++ iron-gcd/src/main.rs
@@ -1,12 +1,22 @@
 /// simple prog to run the web based GCD program.
-/// there is no URL based mapping currently.
+
 extern crate iron;
+extern crate router;
+extern crate urlencoded;
 #[macro_use] extern crate mime;
+
 use iron::prelude::*;
 use iron::status;
+use router::Router;
+use urlencoded::UrlEncodedBody;
+use std::str::FromStr;
+
 fn main() {
+	let mut router = Router::new();
+	router.get("/",get_form,"index");//index won't be used
+	router.post("/gcd",post_gcd,"submit");//submit won't be used
 println!("Serving on http://localhost:3000...");
-Iron::new(get_form).http("localhost:3000").unwrap();
+Iron::new(router).http("localhost:3000").unwrap();
 }
 /*
 Iron::new creates a server , and then sets it to 
@@ -31,4 +41,68 @@ response.set_mut(r#"
 </form>
 "#);
 Ok(response)
+}
+
+fn post_gcd(request: &mut Request)-> IronResult<Response>{
+	let mut response = Response::new();
+	let hashmap;
+
+	match request.get_ref::<UrlEncodedBody>(){
+		Err(e) => {
+			response.set_mut(status::BadRequest);
+			response.set_mut(format!("Error while parsing the form data {:?} \n",e));
+			return Ok(response);
+		}
+		Ok(map) => {
+			hashmap = map;
+		}
+	}
+
+	let unparsed_numbers;
+	match hashmap.get("n"){
+		None => {
+			response.set_mut(status::BadRequest);
+			response.set_mut(format!("form data has no 'n' parameter \n"));
+			return Ok(response);
+		}
+		Some(nums) => {
+			unparsed_numbers = nums;
+		}
+	}
+
+	let mut numbers = Vec::new();
+	for unparsed in unparsed_numbers{
+		match u32::from_str(&unparsed){
+			Err(_)=>{
+				response.set_mut(status::BadRequest);
+				response.set_mut(mime!(Text/Html; Charset=Utf8));
+				response.set_mut(format!("<h3>provided data: {:?} is not a number</h3>",unparsed));
+				return Ok(response);
+			}
+			Ok(n )=>{
+				numbers.push(n);
+			}
+		}	
+	}
+	let mut d = numbers[0];
+	for num in &numbers[1..]{
+		d = gcd(d,*num);
+	}
+
+	response.set_mut(status::Ok);
+	response.set_mut(mime!(Text/Html; Charset=Utf8));
+	response.set_mut(format!("Gcd of numbers : {:?} is <b>{:?}</b>",numbers,d));
+	Ok(response)
+}
+fn gcd(mut x:u32,mut y:u32) -> u32{
+	assert!(x!=0 && y!=0);
+	while x!=0{
+		if x<y {
+			let temp:u32 = x;
+			x=y;
+			y=temp;
+		}
+		x = x%y;
+	}
+	y
 }
